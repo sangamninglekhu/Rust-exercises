b6000ba38ecd0ae5fe40fd6cea53cf0920d5e308
sangam limbu <sangamlimbu52@gmail.com>
2022-02-14 13:14:56 +0000

    first commit

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let (mut missiles, ready) = (STARTING_MISSILES,READY_AMOUNT);
+    // let ready = 2;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
+
+    
+}
