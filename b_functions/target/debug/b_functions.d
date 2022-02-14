1f529f9dc7f8c440259bfa5cbb188612f61ea3b3
sangam limbu <sangamlimbu52@gmail.com>
2022-02-14 13:17:52 +0000

    second function

---
+++ b_functions/target/debug/b_functions.d
@@ -0,0 +1,1 @@
+D:\rust\udemy\ultimate_rust_crash_course\exercise\b_functions\target\debug\b_functions.exe: D:\rust\udemy\ultimate_rust_crash_course\exercise\b_functions\src\main.rs
---
+++ b_functions/target/debug/deps/b_functions.d
@@ -0,0 +1,5 @@
+D:\rust\udemy\ultimate_rust_crash_course\exercise\b_functions\target\debug\deps\b_functions.exe: src\main.rs
+
+D:\rust\udemy\ultimate_rust_crash_course\exercise\b_functions\target\debug\deps\b_functions.d: src\main.rs
+
+src\main.rs:
