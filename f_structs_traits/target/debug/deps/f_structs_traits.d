2145b568edc7ea3a8e0dea190e7c9168a41980d1
sangam limbu <sangamlimbu52@gmail.com>
2022-02-19 12:45:20 +0000

    structs and traits

---
+++ f_structs_traits/target/debug/deps/f_structs_traits.d
@@ -0,0 +1,5 @@
+D:\rust\udemy\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.exe: src\main.rs
+
+D:\rust\udemy\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.d: src\main.rs
+
+src\main.rs:
---
+++ f_structs_traits/target/debug/f_structs_traits.d
@@ -0,0 +1,1 @@
+D:\rust\udemy\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\f_structs_traits.exe: D:\rust\udemy\ultimate_rust_crash_course\exercise\f_structs_traits\src\main.rs
